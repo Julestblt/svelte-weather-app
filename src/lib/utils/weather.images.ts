2c9293f42e960c577f1b05fe4d52f0aa3dee48b1
Jules <jules.thibault2002@gmail.com>
2022-01-13 13:13:59 -0800

    ✨ add weather.images.ts

---
+++ src/lib/utils/weather.images.ts
@@ -0,0 +1,12 @@
+export const imagePath = {
+	sn: { path: '/assets/Snow.png' },
+	sl: { path: '/assets/Sleet.png' },
+	h: { path: '/assets/Hail.png' },
+	t: { path: '/assets/Thunderstorm.png' },
+	hr: { path: '/assets/HeavyRain.png' },
+	lr: { path: '/assets/LightRain.png' },
+	s: { path: '/assets/Shower.png' },
+	hc: { path: '/assets/HeavyCloud.png' },
+	lc: { path: '/assets/LightCloud.png' },
+	c: { path: '/assets/Clear.png' }
+};
