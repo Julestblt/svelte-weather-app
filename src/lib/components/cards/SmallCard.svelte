e2dc891f95b6c02417feda5b5964427ca3787b56
Jules <jules.thibault2002@gmail.com>
2022-01-13 16:21:16 -0800

    ✨ add <SmallCard>

---
+++ src/lib/components/cards/SmallCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let title: string = '';
+
+	export let src: string;
+
+	export let maxDegree: string = null;
+
+	export let minDegree: string = null;
+</script>
+
+<section class="flex items-center justify-between flex-col bg-[#1E213A] p-4">
+	<p>{title}</p>
+	<img {src} alt="" />
+	<div class="flex justify-around w-full">
+		<p>{maxDegree}°C</p>
+		<p>{minDegree}°C</p>
+	</div>
+</section>
