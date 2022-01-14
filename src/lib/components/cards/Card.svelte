2f10ae8ef3a618160289909cfa07a4d211e9f5b8
Jules <jules.thibault2002@gmail.com>
2022-01-13 16:21:21 -0800

    ✨ add <Card>

---
+++ src/lib/components/cards/Card.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<section class="bg-[#1E213A] w-full flex flex-col items-center justify-between p-4 my-6">
+	<p class="text-[16px]">{title}</p>
+	<slot />
+</section>
